8c403f47b47ed0d2aea62f29c27c74d02ba19fd3
Ivan Kozlov <ikozlov9000@gmail.com>
2018-02-27 15:09:13 -0800

    Added proper main

--- main.c
+++ main.c
@@ -6,17 +6,24 @@
 /*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/26 16:37:28 by ikozlov           #+#    #+#             */
-/*   Updated: 2018/02/26 20:58:26 by ikozlov          ###   ########.fr       */
+/*   Updated: 2018/02/27 14:35:44 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <fcntl.h>
 #include "get_next_line.h"
 
 int		main(int ac, char *av[])
 {
-	int		smth;
-	smth = 0;
-	av++;
-	ac++;
+	int		fd;
+	if (ac == 2)
+	{
+			fd = open(av[1], O_RDONLY);
+			char	*line;
+			printf("Fd: %d\n", fd);
+			while (get_next_line(fd, &line))
+				printf("%s\n", line); 
+			close(fd);
+	}
 	return (0);
 }
