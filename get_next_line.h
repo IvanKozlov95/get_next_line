c4894cecb37e741be23bdae6baa1f9e49572ed0e
Ivan Kozlov <ikozlov9000@gmail.com>
2018-02-27 22:49:22 -0800

    Removed unused stuff

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/26 18:54:31 by ikozlov           #+#    #+#             */
-/*   Updated: 2018/02/27 16:29:36 by ikozlov          ###   ########.fr       */
+/*   Updated: 2018/02/27 22:45:09 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,7 @@
 
 # define BUFF_SIZE 222
 # define FD_LIMIT 8192
-# define MAX(a, b) (a > b ? (a) : (b))
 
 int		get_next_line(const int fd, char **line);
-char	*validate_input_file(char *file);
 
 #endif
